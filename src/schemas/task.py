fc3e227efc3f668956a56ae2c977e6aaeb0c6152
bigit22 <koresh.yar@mail.ru>
2025-07-25 16:21:10 +0700

    init alembic + init schemas + renormalize

---
+++ src/schemas/task.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+from src.entities import TaskStatus
+
+
+class TaskBase(BaseModel):
+    title: str = Field(..., max_length=255)
+    description: Optional[str] = None
+    status: TaskStatus = TaskStatus.pending
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = Field(None, max_length=255)
+    description: Optional[str] = None
+    status: Optional[TaskStatus] = None
+
+
+class TaskInDBBase(TaskBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class TaskOut(TaskInDBBase):
+    pass
